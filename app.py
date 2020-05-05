3e42551215cfc105d05bd7866369871e5831df39
PedroBarr203 <pedroenri203@gmail.com>
2020-05-05 18:10:45 -0500

    withDB

---
+++ app.py
@@ -0,0 +1,19 @@
+
+from flask import Flask, render_template, request
+import pandas as pd
+import numpy as np
+
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+
+
+if __name__ == '__main__':
+    app.run(port=9090, debug=True)
