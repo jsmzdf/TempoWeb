fbd3c771ca09b9692912d714eb0f1f8c60f9062d
PedroBarr <pebarreraa@correo.udistrital.edu.co>
2020-05-11 00:39:26 -0500

    ahorasivaenserio1

--- Temporalizador.py
+++ Temporalizador.py
@@ -11,7 +11,6 @@
 
 #importe de librerias
 import threading
-import time
 
 class Temporizador:
     def __init__(self):
@@ -54,49 +53,19 @@ class Temporizador:
     def iniciar(self, t: int, salida = None):
         self.ingresarT(t)
         self.setBreak(True)
-        self.contar()
+        self.contar(salida = salida)
 
-    def contar(self):
+    def contar(self, salida = None):
         if self.getTAct() > 0:
-            threading.Timer(1,lambda: [self.setTAct(self.getTAct() - 1),print(self.getTAct()), self.contar()]).start()
+            threading.Timer(1,lambda: [self.setTAct(self.getTAct() - 1),print(self.getTAct()), salida, self.contar(salida=salida)]).start()
         else:
             self.detener()
 
-    def cont(self,ejer,num=0):
-        seg=(num+1)%60
-        if(seg<ejer)  :
-            return [seg,'ejercicio']
-        else:
-            return [seg,'descanso']
-
-    def doit(self,ejer,tiempo,arg):
-        i=0
-        t = threading.currentThread()
-        while getattr(t, "do_run", True):
-            for i in range(60):
-                time.sleep(1)
-                msg=str(Temp.cont(ejer,i) )
-                print(msg+" minutos:"+str(1))
-            i=i+1
-            if(i==tiempo):
-                return[0,0]                         #t._wait_for_tstate_lock() pausa el hilo
-        return[0,0]
-
 def main():
     t = Temporizador()
-    t.iniciar(5)
+    t.iniciar(5, salida=lambda:[print(t.getTAct())])
     while t.getBreak():
         continue
-    '''
-    ejercicio=30
-    parametro=2
-    t = threading.Thread(target=Temp.doit, args=(ejercicio,parametro,""))
-    t.start()
-    #estoesparamostrarquesedesactivathehilo para que vea que funcia puededarle menos tiempo
-    estoesparamostrarquesedesactivathehilo=parametro=120
-    time.sleep(estoesparamostrarquesedesactivathehilo)
-    t.do_run = False
-    t.join()'''
 
 if __name__ == '__main__':
     main()
