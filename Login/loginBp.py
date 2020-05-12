9881b58d7662037739202a914bd249192dd10693
jsmzdf <sebastianjhon9427@gmail.com>
2020-05-12 18:00:15 -0500

    a

--- Login/loginBp.py
+++ Login/loginBp.py
@@ -17,8 +17,16 @@ from flask_sqlalchemy import SQLAlchemy
 logbp = Blueprint('logbp', __name__, template_folder='templates')#
 
 from app import db
-import ModelosBD as mdb
 
+#configuracion de ruta /login
+@logbp.route('/login', methods=["GET", "POST"])
+def login():
+    return render_template('login.html')
+
+#configuracion de ruta /signin
+@logbp.route('/signin', methods=["GET", "POST"])
+def signin():
+    return render_template('signin.html')
 
 #configuracion de ruta /forgotpass
 @logbp.route('/forgotpass', methods=["GET", "POST"])
@@ -31,8 +39,6 @@ def forgotpass():
 def logout():
     logout_user()
     return redirect(url_for('logbp.login'))
-#configuracion de ruta /login
-
 
 def main():
     pass
