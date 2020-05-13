07616228a09a9cf7c5a3076a55b0fcc9af92dcd8
Sebastian-MG <sebastianmancera12@gmail.com>
2020-05-12 20:13:12 -0500

    Se realiza el Login

--- Login/loginBp.py
+++ Login/loginBp.py
@@ -1,5 +1,5 @@
-#-------------------------------------------------------------------------------
-# Name:        Login con BluePrint
+﻿#-------------------------------------------------------------------------------
+# Name:        tengo hambre
 # Purpose:     Sacarle nota a Daza dospuntos(w/2)
 #
 # Author:      Alguien
@@ -16,17 +16,100 @@ from flask_sqlalchemy import SQLAlchemy
 
 logbp = Blueprint('logbp', __name__, template_folder='templates')#
 
-from app import db 
-
-#configuracion de ruta /login
+from app import db
+import ModelosBD as mdb
 @logbp.route('/login', methods=["GET", "POST"])
+@login_required
 def login():
-    return render_template('login.html')
+    if 'mess' not in session:
+        session['mess']={}
+    if current_user.is_authenticated:
+        return redirect(url_for('Rutina'))
+    if "usuario_temp" in session:
+        session["usuario_temp"] = {}
+    if (request.method=="GET"):
+        return render_template('login.html')
+    else:
+        try:
+            usu = request.form['nmUserLog']
+            passuslg = request.form['psUserLog']
+        except:
+            #session['mess']['alertas'].append("Fallo en el envio de datos. Revise los campos.")
+            flash("Fallo en el envio de datos. Revise los campos.")
+            return redirect(url_for('login'))
+        #passlg_encode = passlg.encode("utf-8")
 
-#configuracion de ruta /signin
+        try:
+            userFnd = mdb.usuario.query.filter_by(nom_usu=usu).first()
+        except:
+            #session['mess']['alertas'].append("Usuario no encontrado. Revise el campo de email.")
+            flash("Usuario no encontrado.")
+            return redirect(url_for('login'))
+
+        if userFnd!=None:
+            if userFnd.contra_usu == passuslg:
+                login_user(userFnd)
+                return redirect(url_for('login'))
+            else:
+                #session['mess']['alertas'].append("Contraseña incorrecta. Revise el campo de contraseña.")
+                flash("Contraseña incorrecta. Revise el campo de contraseña.")
+                return redirect(url_for('login'))
+        else:
+            #session['mess']['alertas'].append("Usuario no encontrado. Revise el campo de email.")
+            flash("Usuario no encontrado.")
+            return redirect(url_for('login'))
+
 @logbp.route('/signin', methods=["GET", "POST"])
+@login_required
 def signin():
-    return render_template('signin.html')
+    if 'mess' not in session:
+        session['mess']={}
+    if current_user.is_authenticated:
+        return redirect(url_for('login'))
+    if (request.method=="GET"):
+        if "usuario_temp" in session:
+            for dat in ["name","last"]:
+                if dat not in session["usuario_temp"]: session["usuario_temp"][dat]=""
+            if "imag" not in session["usuario_temp"]:
+                session["usuario_temp"]["imag"]="http://adoracionysanidad.herokuapp.com/static/recourses/icon-user.png"
+        else:
+            session["usuario_temp"]={}
+            session["usuario_temp"]["name"] = ""
+            session["usuario_temp"]["last"] = ""
+
+        return render_template('signin.html')
+    else:
+        allright = True
+        scopes = ["name","last"]
+        spaces = ['userNom', 'userApe']
+        signif = ['nombre', 'apellido']
+        for dat in range(len(scopes)):
+            if spaces[dat] in request.form and request.form[spaces[dat]] != "": session["usuario_temp"][scopes[dat]]=request.form[spaces[dat]]
+            else:
+                allright = False
+                flash("Revise el campo de %s" % signif[dat])
+        scopes = ["prof","alma"]
+        spaces = ['userPrf', 'userUni']
+        for dat in range(len(scopes)):
+            if spaces[dat] in request.form: session["usuario_temp"][scopes[dat]]=request.form[spaces[dat]]
+        if 'correo' in request.form:
+            usMail = request.form['correo']
+            if db.search('', usMail) is None:
+                allright = False
+                flash("Revise el campo de email, no es un formato correcto")
+            else:
+                session["usuario_temp"]["mail"] = usMail
+        if 'usu' in request.form:
+            usPhon = request.form['usu']
+            if db.search('', usPhon) is None:
+                allright = False
+                flash("Revise el campo de ususario, no es un formato correcto")
+            else:
+                session["usuario_temp"]["phon"] = usPhon
+
+        else:
+            flash("Ya existe ese email de usuario, prueba en iniciar sesion")
+        return redirect(url_for('login'))
 
 #configuracion de ruta /forgotpass
 @logbp.route('/forgotpass', methods=["GET", "POST"])
